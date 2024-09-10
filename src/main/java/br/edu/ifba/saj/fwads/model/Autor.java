e1807e8a118e7eab36f53c8a523a423411a9d5bc
Leandro Costa <leandro.costa@ifba.edu.br>
2024-09-10 18:24:31 -0300

    step 2

--- src/main/java/br/edu/ifba/saj/fwads/model/Autor.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/Autor.java
@@ -1,13 +1,19 @@
 package br.edu.ifba.saj.fwads.model;
 
 public class Autor {
+    private Long id;
+    private static Long idGenerator = 0l;
     private String nome;
     private String email;
     private String CPF;
     public Autor(String nome, String email, String cPF) {
+        this.id = ++idGenerator;
         this.nome = nome;
         this.email = email;
         CPF = cPF;
+    }
+    public Long getId() {
+        return id;
     }
     public String getNome() {
         return nome;
@@ -29,15 +35,13 @@ public class Autor {
     }
     @Override
     public String toString() {
-        return "Autor [nome=" + nome + ", email=" + email + ", CPF=" + CPF + "]";
+        return "Autor [id=" + id + ", nome=" + nome + ", email=" + email + ", CPF=" + CPF + "]";
     }
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((CPF == null) ? 0 : CPF.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
     @Override
@@ -49,20 +53,10 @@ public class Autor {
         if (getClass() != obj.getClass())
             return false;
         Autor other = (Autor) obj;
-        if (nome == null) {
-            if (other.nome != null)
+        if (id == null) {
+            if (other.id != null)
                 return false;
-        } else if (!nome.equals(other.nome))
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
-        if (CPF == null) {
-            if (other.CPF != null)
-                return false;
-        } else if (!CPF.equals(other.CPF))
+        } else if (!id.equals(other.id))
             return false;
         return true;
     }
--- src/main/java/br/edu/ifba/saj/fwads/model/Livro.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/Livro.java
@@ -1,16 +1,24 @@
 package br.edu.ifba.saj.fwads.model;
 
 public class Livro {
+    private Long id;
+    private static Long idGenerator = 0l;
+
     private String titulo;
     private String subTitulo;
     private String ISBN;
     private Autor autor;
 
     public Livro(String titulo, String subTitulo, String iSBN, Autor autor) {
+        this.id = ++idGenerator;
         this.titulo = titulo;
         this.subTitulo = subTitulo;
         ISBN = iSBN;
         this.autor = autor;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitulo() {
@@ -47,6 +55,36 @@ public class Livro {
 
     @Override
     public String toString() {
-        return "Livro [titulo=" + titulo + ", subTitulo=" + subTitulo + ", ISBN=" + ISBN + ", autor=" + autor + "]";
+        return "Livro [id=" + id + ", titulo=" + titulo + ", subTitulo=" + subTitulo + ", ISBN=" + ISBN + ", autor="
+                + autor + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+    
+
 }
